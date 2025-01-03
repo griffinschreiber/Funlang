ada8de8f9c2c0cce5999b94736cd7b56f1c03c0a
hackleberry <griffinschreiber@gmail.com>
2025-01-03 12:20:51 +0000

    Worked more on arena implementation

--- include/utils/arena.h
+++ include/utils/arena.h
@@ -9,8 +9,8 @@ struct arena *make_arena();
 
 void *arena_alloc(size_t alloc_size);
 
-void reset_arena(struct arena *arena);
+void arena_reset(struct arena *arena);
 
-void free_arena(struct arena *arena);
+void arena_free(struct arena *arena);
 
 #endif // ARENA_H_
--- src/utils/arena.c
+++ src/utils/arena.c
@@ -1,12 +1,29 @@
 #include "arena.h"
 
-#define ARENA_BLOCK_DATA_SIZE 1024
+#include <stdio.h>
+
+#define ARENA_BLOCK_CAPACITY 1024
+#define ARENA_MAX_BLOCKS 4
 
 struct block {
-  char data[ARENA_BLOCK_DATA_SIZE];
+  char data[ARENA_BLOCK_CAPACITY];
   size_t index;
   struct block *next;
 };
+
+// go nom nom on the heap.
+struct block *make_block() {
+  struct block *block = (struct block *)malloc(sizeof(struct block));
+  block->index = 0;
+  block->next = NULL;
+  return block;
+}
+
+void free_block(struct arena *arena, struct block *block) {
+  block->index = 0;
+  block->next = arena->free;
+  arena->free = block;
+}
 
 struct arena {
   struct block *active;
@@ -16,12 +33,42 @@ struct arena {
 struct arena make_arena() {
   struct arena arena;
   arena.active = NULL;
-  arena.free = (struct block *)malloc(sizeof(struct block));
+  arena.free = make_block();
   return &arena;
 }
 
-void free_block(struct arena *arena, struct block *block) {
-  block->index = 0;
-  block->next = arena->free;
-  arena->free = block;
+void *arena_alloc(struct arena *arena, size_t alloc_size) {
+  if (alloc_size > ARENA_BLOCK_CAPACITY) {
+    fprintf(stderr, "arena_alloc: called with alloc_size bigger than ARENA_BLOCK_CAPACITY.");
+    exit(1);
+  }
+  struct block *current = arena->free;
+  while (ARENA_BLOCK_CAPACITY - current->index < alloc_size) {
+    if (current->next == NULL) {
+      current->next = make_block();
+      return current->next;
+    }
+    current = current->next;
+  }
+  return current->data + current->index;
+}
+
+void arena_reset(struct arena *arena) {
+  struct block *current = active;
+  struct block *next;
+  while (current) {
+    next = current;
+    free_block(current);
+    current = next;
+  }
+}
+
+void arena_free(struct arena *arena) {
+  struct block *current = active;
+  struct block *next;
+  while (current) {
+    next = current;
+    free(current);
+    current = next;
+  }
 }
